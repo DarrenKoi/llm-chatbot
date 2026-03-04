6957f4a5ba9682cd4144e20e3275e93089b55237
DarrenKoi <daeyoung1385@gmail.com>
2026-03-04 10:39:05 +0900

    index: FLASK_PORT 의존 제거

--- index.py
+++ index.py
@@ -3,5 +3,4 @@ from api import create_application
 app = create_application()
 
 if __name__ == "__main__":
-    from api import config
-    app.run(host="0.0.0.0", port=config.FLASK_PORT)
+    app.run(host="0.0.0.0")
